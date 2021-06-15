5c40b2710a27e00ea131d98f159bdf13350a1a51
sunkyung95 <37947951+sunkyung95@users.noreply.github.com>
2021-06-15 22:50:17 +0900

    액티비티 사이에 값 주고받기
    p307 ~ p319

--- Activity/app/src/main/java/kr/co/hanbit/activity/MainActivity.kt
+++ Activity/app/src/main/java/kr/co/hanbit/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package kr.co.hanbit.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kr.co.hanbit.activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,41 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SubActivity::class.java)  // 인텐스 생성
                                                 // 호출할 클래스명 뒤에 '::class.java'입력해야 함
 
-        binding.btnStart.setOnClickListener { startActivity(intent) }
+        intent.putExtra("from1", "Hello Bundle")
+        intent.putExtra("from2", 2021)
+        // putExtra() 메서드 사용하여 intent에 값 전달   키, 값
+        // putExtra는 여러 타입을 담을 수 있음
+
+        // binding.btnStart.setOnClickListener { startActivity(intent) }
+        binding.btnStart.setOnClickListener { startActivityForResult(intent, 99) }
+    }
+    
+    /*
+        onActivityResult() 메서드의 구조
+        1) requestCode: 호출 시에 메인 액티비티에서 입력하는 코드 startActivityForResult 메서드에 인텐트와 함께 입력해서 호출한 코드를 구분함
+        2) resultCode: 결과 처리 후 서브 액티비티에서 입력하는 코드, 앞에서 RESULT_OK를 담아서 보냈음
+        3) data: 결과 처리 후 서브 액티비티가 넘겨주는 인텐트가 담겨있음
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        
+        if (resultCode == RESULT_OK) {  // 서브 액티비티에서 돌려받은 resultCode가 정상인지 체크하는 코드
+            val message = data?.getStringExtra("returnValue")
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show() // 해당 메세지를 토스트로 화면에 보여주는 코드
+            // Toast.makeText의 파라미터
+            // 1) 첫번째: 화면을 위한 기본 도구인 컨텍스트가 필요한데, 액티비티가 이미 가지고 있기때문에
+            //            this라고 입력하면 됨
+            // 2) 두번째: 출력될 메시지를 문자열로 전달
+            // 3) 세번째: 메시지가 얼마 동안 출력될지를 결정
+
+            if(resultCode == Activity.RESULT_OK) {  // requestCode가 요청 코드와 같은 99인지 체크
+                when (requestCode) {
+                    99 -> {
+                        val message = data?.getStringExtra("returnValue")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 }
--- Activity/app/src/main/java/kr/co/hanbit/activity/SubActivity.kt
+++ Activity/app/src/main/java/kr/co/hanbit/activity/SubActivity.kt
@@ -1,11 +1,29 @@
 package kr.co.hanbit.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.hanbit.activity.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySubBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        setContentView(binding.root)
+
+        binding.to1.text = intent.getStringExtra("from1")
+        binding.to2.text = "${intent.getIntExtra("from2", 0)}"
+        // get***Extra()메서드 사용하여 to1의 text에 인텐트에 담겨온 값을 from1키로 꺼내서 입력함
+        // text속성은 문자열만 받기 때문에 "${}"을 사용하여 문자열로 변환해줌
+
+        binding.btnClose.setOnClickListener {   // 액티비티 닫기 버튼 선택시 실행될 내용
+            val returnIntent = Intent()
+            returnIntent.putExtra("returnValue", binding.editMessage.text.toString())
+            setResult(RESULT_OK, returnIntent)  // 처리한 결과값이 성공이면 returnIntent에 담긴 값을 전달함
+            finish()    // 액티비티 종료 -> 메인 액티비티에 값이 전달됨
+        }
     }
 }
